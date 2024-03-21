e1fb33c146e17f33972118118271f00e04f6bed1
Harun Meşe <81909205+harun-mese@users.noreply.github.com>
2024-03-21 19:21:00 +0000

    7

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,44 @@ const formCloseBtn = document.getElementById('close_form_btn')
 const loginForm = document.getElementById('login_form')
 const articles = document.getElementById('articles')
 const contents = document.getElementById('contents')
+
+const contentHref = document.querySelectorAll("#contents a")
+const articlesHref = document.querySelectorAll("#articles a")
+
 let ARICLES_STATUS = false
 let CONTENTS_STATUS = false
 
+
+
+contentHref.forEach(i=>{
+    i.addEventListener('click',()=>{
+        toggleContent()
+
+    })
+})
+articlesHref.forEach(i=>{
+    i.addEventListener('click',()=>{ toggleArticle() })
+})
+
 toggleArticles.addEventListener('click',()=>{
+     toggleArticle()
+     if (CONTENTS_STATUS) {
+        toggleContent()
+     }
+
+    })
+toggleContents.addEventListener('click',()=>{
+     toggleContent()
+     if (ARICLES_STATUS) {
+        toggleArticle()
+     }
+    })
+
+
+function toggleArticle() {
     if (ARICLES_STATUS) {
         toggleArticles.classList.toggle('open')
-        articles.style.left = 'calc(-100vw + 35px)'
+        articles.style.left = '-100vw'
         articles.style.zIndex = 3
         ARICLES_STATUS = false
     }
@@ -22,22 +53,23 @@ toggleArticles.addEventListener('click',()=>{
         articles.style.zIndex = 5
         ARICLES_STATUS = true
     }
-})
-
-toggleContents.addEventListener('click',()=>{
+}
+function toggleContent() {
     if (CONTENTS_STATUS) {
         toggleContents.classList.toggle('open')
-        contents.style.right = 'calc(-100vw + 35px)'
+        contents.style.left = '-100vw'
         contents.style.zIndex = 3
         CONTENTS_STATUS = false
     }
     else{
         toggleContents.classList.toggle('open')
-        contents.style.right = '0px'
+        contents.style.left = '0px'
         contents.style.zIndex = 5
         CONTENTS_STATUS = true
     }
-})
+}
+
+
 formCloseBtn.addEventListener('click',()=>{
     loginForm.style.display='none'
 })
@@ -45,3 +77,32 @@ formCloseBtn.addEventListener('click',()=>{
 function open_form(){
     loginForm.style.display='flex'
 }
+
+window.addEventListener('scroll', function() {
+    var headers = document.querySelectorAll('article h1, article h2, article h3, article h4, article h5, article h6');
+
+    for (var i = 0; i < headers.length; i++) {
+      var currentHeader = headers[i];
+      var nextHeader = headers[i + 1];
+      var currentHeaderRect = currentHeader.getBoundingClientRect();
+      var nextHeaderRect = nextHeader.getBoundingClientRect();
+      
+      if (currentHeaderRect.bottom >= nextHeaderRect.top) {
+        currentHeader.style.position = 'static';
+        nextHeader.style.position = 'sticky';
+        nextHeader.style.textWrap = 'nowrap';
+        nextHeader.style.overflow = 'hidden';
+      }
+      else if (currentHeaderRect.top >= nextHeaderRect.bottom) {
+        currentHeader.style.position = 'sticky';
+        nextHeader.style.position = 'static';
+        nextHeader.style.textWrap = 'wrap';
+        nextHeader.style.overflow = 'vissible';
+      }
+      else {
+        nextHeader.style.position = 'sticky';
+        nextHeader.style.textWrap = 'wrap';
+        nextHeader.style.overflow = 'vissible';
+      }
+    }
+  });
